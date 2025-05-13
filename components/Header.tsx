dabdb538dee74fe5238b33f3a24534020b8661a7
DeveloperIRkhan <programmer.irfansha@hotmail.com>
2025-05-13 22:55:24 +0500

    Shadui work is initiated

---
+++ components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Container from "./Container";
+
+const Header = () => {
+  return (
+    <Container>
+      <header className="">Header</header>
+    </Container>
+  );
+};
+
+export default Header;
